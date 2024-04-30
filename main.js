ceb3c11f12289513bd8b555cd672aba82c36f7d8
Igniske <ignaciomanuel07@gmail.com>
2024-04-29 23:50:49 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,17 @@
+
+
+
+document.querySelector('#app').innerHTML = `
+  <div>
+    <div id="navbar"></div>
+    <div style="background-image: url('/abogacia.jpg');" class="h-screen bg-cover opacity-70"></div>
+  </div>
+`
+
+
+document.querySelector('#navbar').innerHTML = `
+  <div>
+    <nav class="fixed top-0 left-0 w-full z-10 bg-white bg-opacity-5">
+        <div class="text-5xl">Nombre</div>
+    </nav>
+  </div>`
