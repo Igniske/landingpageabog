5167bd1763cc31db9172b2366ce7163a9b0ef37e
Igniske <104842998+Igniske@users.noreply.github.com>
2024-05-02 10:59:46 -0300

    feat: se remueve caja de comentarios, se agregan estilos

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+
+
+
 document.querySelector('#app').innerHTML = `
   <div class="relative">
     <div id="navbar"></div>
@@ -10,54 +13,60 @@ document.querySelector('#app').innerHTML = `
       </div>
     </div>
     <div class="h-full bg-black flex flex-col space-y-8 justify-center items-center m-auto">
-      <div class="flex flex-col justify-center items-center">
-        <div class="h-16 w-16 border-white border-2">
-        </div>
-        <p class="text-white text-center">Texto de relleno</p>
-      </div>
-      <div class="flex flex-col justify-center items-center">
-        <div class="h-16 w-16 border-white border-2">
-        </div>
-        <p class="text-white text-center">Texto de relleno</p>
-      </div>
-      <div class="flex flex-col justify-center items-center">
-        <div class="h-16 w-16 border-white border-2">
-        </div>
-        <p class="text-white text-center">Texto de relleno</p>
-      </div>
-      <div class="flex flex-col justify-center items-center">
-        <div class="h-16 w-16 border-white border-2">
-        </div>
-        <p class="text-white text-center">Texto de relleno</p>
-      </div>
+      
+    <div class="flex flex-col justify-center items-center relative container">
+  <div class="h-16 w-16 border-white border-2"></div>
+  <h3 class="text-white hover:text-red-900 cursor-pointer text-3xl text-center toggle-text">Ciberseguridad</h3>
+  <div class="w-1/2">
+    <p class="text-white hidden-text text-justify">Los asesoramos en la prevención y gestión de brechas de seguridad de la información a fin de prevenir ataques cibernéticos mediante ransomware, malware o cualquier otro tipo de software. Analizamos el estado de situación de tu organización y realizamos la política de ciberseguridad que mejor se ajusta a tus necesidades.</p>
+  </div>
+</div>
+
+<div class="flex flex-col justify-center items-center relative container">
+  <div class="h-16 w-16 border-white border-2"></div>
+  <p class="text-white cursor-pointer text-center toggle-text">Proteccion y privacidad</p>
+  <p class="text-white hidden-text">Te ayudamos con todo lo necesario para cumplir con los estándares necesarios en la materia. Los acompañamos en el diseño de nuevos productos y servicios con la metodología de privacidad desde el diseño.</p>
+</div>
+
+
+
 
       <div class="md:w-3/5">
         <p class="text-2xl font-bold text-white">Si quieres conocer más sobre nuestro productos y servicios contáctanos</p>
       </div>
     </div>
 
-    <div class="h-full bg-black flex flex-col space-y-8 justify-center items-center m-auto" id="formContainer">
-      <div class="flex flex-col justify-center items-center">
-        <input type="text" class="input-field" placeholder="Nombre y Apellido">
-      </div>
-      <div class="flex flex-col justify-center items-center">
-        <input type="text" class="input-field" placeholder="Teléfono">
-      </div>
-      <div class="flex flex-col justify-center items-center">
-        <input type="email" class="input-field" placeholder="Email">
-      </div>
-      <div class="flex flex-col justify-center items-center">
-        <textarea class="input-field" rows="4" placeholder="Comentario"></textarea>
-      </div>
-      <button class="bg-blue-500 text-white px-4 py-2 rounded">Enviar</button>
+    <div class="gradient-bg flex justify-center items-center h-full">
+      <div class="text-white text-center">
+        <h2 class="text-4xl font-bold">NOMBRE</h2>
+        <p class="mt-4">Texto de relleno para poner a prueba el funcionamiento de este div</p>
+        </div>
     </div>
-  </div>
+    <div class="bg-red-800 w-full flex justify-center items-center">
+      <p class="text-xl text-gray-600">Nombre 2024 | All rights reserved</p>
+    </div>
+</div>
+
+
 `;
 
 document.querySelector('#navbar').innerHTML = `
   <div>
     <nav class="absolute top-0 left-0 w-full z-10 bg-white bg-opacity-5">
-      <div class="text-5xl ml-4 my-8 md:ml-72">Nombre</div>
+      <div class="text-5xl ml-4 my-8 md:ml-72">NOMBRE</div>
     </nav>
   </div>
 `;
+
+
+document.querySelectorAll('.container').forEach(container => {
+  container.addEventListener('click', function(event) {
+    const toggleText = container.querySelector('.toggle-text');
+    const hiddenText = container.querySelector('.hidden-text');
+    if (event.target === toggleText) {
+      const currentDisplay = window.getComputedStyle(hiddenText).display;
+      hiddenText.style.display = currentDisplay === 'none' ? 'block' : 'none';
+    }
+  });
+});
+
